721b73b89c0d5b32fc73da19eacdbd0134928e89
CodeOmari <jacksonomari2230@gmail.com>
2024-11-20 19:10:08 +0300

    project models

--- main/models.py
+++ main/models.py
@@ -1,4 +1,12 @@
+import os
+import uuid
+
 from django.db import models
+
+def generate_unique_name(instance, filename):
+    name = uuid.uuid4() #
+    full_file_name = f'{name}-{filename}'
+    return os.path.join("profile_pictures", full_file_name)
 
 # Create your models here.
 class Customer(models.Model):
@@ -7,6 +15,7 @@ class Customer(models.Model):
     email = models.EmailField(unique=True)
     dob = models.DateField()
     gender = models.CharField(max_length=10)
+    profile_pic = models.ImageField(upload_to=generate_unique_name, null=True)
     weight = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
